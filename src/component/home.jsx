0179349400123a12dc11c50ef86885de4fb41678
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-09-12 20:02:19 -0300

    Actualización

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -11,60 +11,58 @@ const styleDiv = {
   minHeight: "100vh",
 };
 
-export class Home extends React.Component {
-  render() {
-    return (
-      <div style={styleDiv}>
-        <div className="mx-5 my-5 px-5 pt-5">
-          <Jumbotron />
-          <div className="row text-center">
-            <div className="col-sm-3">
-              <Card
-                imageUrl="https://via.placeholder.com/500x325"
-                cardTitle="Card Title"
-                content="It is a long established fact that a reader 
+export const Home = () => {
+  return (
+    <div style={styleDiv}>
+      <div className="mx-5 my-5 px-5 pt-5">
+        <Jumbotron />
+        <div className="row text-center">
+          <div className="col-sm-3">
+            <Card
+              imageUrl="https://via.placeholder.com/500x325"
+              cardTitle="Card Title"
+              content="It is a long established fact that a reader 
 							will be distracted by the readable content of a page when 
 							looking at its layout."
-                url="#"
-                label="Find Out More!"
-              />
-            </div>
-            <div className="col-sm-3">
-              <Card
-                imageUrl="https://via.placeholder.com/500x325"
-                cardTitle="Card Title"
-                content="There are many variations of passages of 
+              url="#"
+              label="Find Out More!"
+            />
+          </div>
+          <div className="col-sm-3">
+            <Card
+              imageUrl="https://via.placeholder.com/500x325"
+              cardTitle="Card Title"
+              content="There are many variations of passages of 
 							Lorem Ipsum available, but the majority have suffered 
 							alteration in some form, "
-                url="#"
-                label="Find Out More!"
-              />
-            </div>
-            <div className="col-sm-3">
-              <Card
-                imageUrl="https://via.placeholder.com/500x325"
-                cardTitle="Card Title"
-                content="Lorem ipsum dolor sit amet, consectetur 
+              url="#"
+              label="Find Out More!"
+            />
+          </div>
+          <div className="col-sm-3">
+            <Card
+              imageUrl="https://via.placeholder.com/500x325"
+              cardTitle="Card Title"
+              content="Lorem ipsum dolor sit amet, consectetur 
 							adipiscing elit, sed do eiusmod tempor incididunt ut labore 
 							et dolore magna aliqua."
-                url="#"
-                label="Find Out More!"
-              />
-            </div>
-            <div className="col-sm-3">
-              <Card
-                imageUrl="https://via.placeholder.com/500x325"
-                cardTitle="Card Title"
-                content="Sed ut perspiciatis unde omnis iste natus 
+              url="#"
+              label="Find Out More!"
+            />
+          </div>
+          <div className="col-sm-3">
+            <Card
+              imageUrl="https://via.placeholder.com/500x325"
+              cardTitle="Card Title"
+              content="Sed ut perspiciatis unde omnis iste natus 
 							error sit voluptatem accusantium doloremque laudantium."
-                url="#"
-                label="Find Out More!"
-              />
-            </div>
+              url="#"
+              label="Find Out More!"
+            />
           </div>
         </div>
-        <Footer />
       </div>
-    );
-  }
-}
+      <Footer />
+    </div>
+  );
+};
