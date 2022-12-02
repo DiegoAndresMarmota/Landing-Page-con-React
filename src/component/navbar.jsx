f70ce311c10f89109ccffa5b22a840cf6403fe2e
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-02 19:05:03 -0300

    Corrección de Navbar

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,49 +1,79 @@
 import React from "react";
 
-//create your first component
-export const NavBar = () => {
-  return (
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">
-          Start Bootstrap
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="d-flex align-items-end navbar-nav navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/acerca_de">
-                About
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/contacto">
-                Services
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/acerca_de">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+export const Navbar = () => {
+	return (
+		<>
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<div className="container">
+					<a className="navbar-brand" href="#">
+						Navbar
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNavDropdown"
+						aria-controls="navbarNavDropdown"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div
+						className="collapse navbar-collapse"
+						id="navbarNavDropdown">
+						<ul className="navbar-nav">
+							<li className="nav-item">
+								<a
+									className="nav-link active"
+									aria-current="page"
+									href="#">
+									Home
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">
+									Features
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">
+									Pricing
+								</a>
+							</li>
+							<li className="nav-item dropdown">
+								<a
+									className="nav-link dropdown-toggle"
+									href="#"
+									id="navbarDropdownMenuLink"
+									role="button"
+									data-bs-toggle="dropdown"
+									aria-expanded="false">
+									Dropdown link
+								</a>
+								<ul
+									className="dropdown-menu"
+									aria-labelledby="navbarDropdownMenuLink">
+									<li>
+										<a className="dropdown-item" href="#">
+											Action
+										</a>
+									</li>
+									<li>
+										<a className="dropdown-item" href="#">
+											Another action
+										</a>
+									</li>
+									<li>
+										<a className="dropdown-item" href="#">
+											Something else here
+										</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
 };
