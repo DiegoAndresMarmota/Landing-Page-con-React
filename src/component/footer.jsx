34ee2966aa4ec3f29225026207f3948df57d885a
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-02 19:05:44 -0300

    Corrección de Footer

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 
 export const Footer = () => {
-  const styleFooter = {
-    marginTop: "auto",
-  };
-
-  return (
-    <footer className="py-5 bg-dark footer" style={styleFooter}>
-      <div className="container-fluid">
-        <p className="m-auto text-center text-white">
-          Copyright © Your Website 2022
-        </p>
-      </div>
-    </footer>
-  );
+	return (
+		<>
+			<div className="container-fluid bg-dark p-3">
+				<p className="text-center text-white">
+					Copyright © Your Website - 2022
+				</p>
+			</div>
+		</>
+	);
 };
