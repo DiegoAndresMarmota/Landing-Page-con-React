4073709d1b4a70d19ab1f7f67f2c1f03803b6a8f
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-02 19:05:18 -0300

    Corrección de Jumbotron

--- src/component/jumbotron.jsx
+++ src/component/jumbotron.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
-export const Jumbotron = () => (
-  <div className="jumbotron">
-    <h1 className="display-4">A Warm Welcome!</h1>
-    <p className="lead">
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry standard dummy text ever since the 1500s
-    </p>
-    <a className="btn btn-primary btn-lg" href="/" role="button">
-      Call to action!
-    </a>
-  </div>
-);
+export const Jumbotron = () => {
+  return (
+    <>
+      <section className="py-5 container bg-secondary">
+        <div className="row py-lg-5">
+          <div className="col-lg-6 col-md-8">
+            <h1 className="fw-light">A warm Welcome</h1>
+            <p className="text-dark">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy.
+            </p>
+            <p>
+              <a href="/" className="btn btn-primary">
+                Call to action!
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
